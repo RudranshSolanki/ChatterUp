d7a79a233df361cf84e53964cc73a3196a8ac252
RudranshSolanki <rudrasingh7318115@gmail.com>
2025-01-24 20:11:41 +0530

    add features
    - added connected users list
    - notify other user when some left the chat or join the chat

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
         methods:['GET', 'POST']
     }
 });
-
+let users= [];
 io.on('connection', (socket) => {
 
     console.log('socket connected');
@@ -22,18 +22,32 @@ io.on('connection', (socket) => {
         console.log(userData);
         const msg = new MessageModel(userData);
         await msg.save();
-        socket.broadcast.emit('message', userData.message);
+        socket.broadcast.emit('message', userData);
     });
     socket.on('userjoin',async(user)=>{
         console.log(user+" joined");
         socket.broadcast.emit('newuser', user);
-        socket.emit('hello', user);   
         console.log('new user joined');
+        users[socket.id]=user;
+        io.emit('update-users',Object.values(users));
         socket.emit('loadmessages',await MessageModel.find());
     });
-    socket.on('working',()=>{
-        console.log('working');
+    socket.on('typing',(user)=>{
+        socket.broadcast.emit('broadcasttyping',user);
     })
+    socket.on('stoptyping',(user)=>{
+        socket.broadcast.emit('broadcaststoptyping',user);
+    })
+    socket.on('disconnect',()=>{
+        
+
+        const username = users[socket.id];
+        console.log(username+' disconnected');
+        delete users[socket.id];
+        socket.broadcast.emit('userleft',username);
+        io.emit('update-users',Object.values(users));
+
+    });
 });
 
 server.listen(3000, () => {
